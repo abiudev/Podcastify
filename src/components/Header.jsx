f044ed3ff5c12fccec81f726111fc48ff8bb656e
Dev Abiud <136119623+abiudev@users.noreply.github.com>
2024-10-29 04:59:33 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,86 @@
 import { useState } from "react";
-import SearchBar from "./SearchBar";
-
+import SearchBar from "./SearchBar"; // Original SearchBar component
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import SignIn from "./SignIn.jsx";
 
 export default function Header({
   handleSearchChange,
   handleSearchClick,
   searchTerm,
   shows,
+  handleLogin,
 }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <nav className="bg-green-500 p-4 z-0">
-        <div className="container mx-auto flex items-center justify-between">
-          {/* Menu Items */}
-          <div className="flex items-center">
-            <div className="hidden md:flex space-x-4">
-              <a href="#" className="text-white hover:text-gray-300">
+        <div className="container mx-auto flex items-center justify-between p-1">
+          <div className="flex items-center ml-0">
+            <img src="/logo.png" alt="logo" className="h-18 w-16" />
+            {/* Menu items next to logo in desktop mode */}
+            <div className="hidden md:flex space-x-4 ml-4">
+              <a
+                href="#"
+                className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+              >
                 Home
               </a>
-              <a href="#" className="text-white hover:text-gray-300">
+              <a
+                href="#"
+                className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+              >
                 About
               </a>
-              <a href="#" className="text-white hover:text-gray-300">
+              <a
+                href="#"
+                className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+              >
                 Top
+              </a>
+              <a
+                href="#"
+                className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+                onClick={handleLogin}
+              >
+                Sign In
               </a>
             </div>
           </div>
 
-          <div className="hidden md:block">
-            <SearchBar
-              handleSearchChange={handleSearchChange}
-              handleSearchClick={handleSearchClick}
-              searchTerm={searchTerm}
-              shows={shows}
-            />
+          {/* Conditional Rendering for Search Bars */}
+          <div className="flex-grow flex justify-center mt-4 md:mt-0">
+            {/* Original Search Bar for Desktop */}
+            <div className="hidden md:block w-full max-w-xs">
+              <SearchBar
+                handleSearchChange={handleSearchChange}
+                handleSearchClick={handleSearchClick}
+                searchTerm={searchTerm}
+                shows={shows}
+              />
+            </div>
+
+            {/* Smaller Search Bar for Mobile */}
+            <div className="md:hidden relative w-full mr-3  mb-3 max-w-xs">
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search..."
+                className="border rounded-full py-1 px-3 focus:outline-none focus:ring focus:ring-green-300 transition duration-150 ease-in-out w-full"
+              />
+              <button
+                onClick={handleSearchClick}
+                className="absolute right-1 top-1/2 transform -translate-y-1/2 text-white bg-green-500 rounded-full p-1"
+              >
+                Search
+              </button>
+            </div>
           </div>
 
-          <div className="flex space-x-4 items-center">
-            <button
-              className="md:hidden text-white"
-              onClick={() => setIsOpen(!isOpen)}
-            >
+          {/* Hamburger menu icon for mobile view */}
+          <div className="flex items-center justify-end md:hidden">
+            <button className="text-white" onClick={() => setIsOpen(!isOpen)}>
               {isOpen ? (
                 <XIcon className="h-6 w-6" />
               ) : (
@@ -53,31 +90,29 @@ export default function Header({
           </div>
         </div>
 
-        <div className="block md:hidden mt-4">
-          {isOpen && (
-            <SearchBar
-              handleSearchChange={handleSearchChange}
-              handleSearchClick={handleSearchClick}
-              searchTerm={searchTerm}
-              shows={shows}
-            />
-          )}
-        </div>
+        {isOpen && (
+          <div className="bg-green-300 flex flex-col p-4 md:hidden">
+            <a
+              href="#"
+              className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-gray-950 font-roboto-condensed font-bold hover:text-gray-600"
+            >
+              Top
+            </a>
+          </div>
+        )}
       </nav>
-
-      {isOpen && (
-        <div className="bg-gray-300 p-4 md:hidden">
-          <a href="#" className="block text-white hover:text-gray-300 mb-2">
-            Home
-          </a>
-          <a href="#" className="block text-white hover:text-gray-300 mb-2">
-            About
-          </a>
-          <a href="#" className="block text-white hover:text-gray-300 mb-2">
-            Services
-          </a>
-        </div>
-      )}
     </>
   );
 }
